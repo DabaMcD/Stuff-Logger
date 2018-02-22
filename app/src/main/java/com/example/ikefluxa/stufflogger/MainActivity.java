8d027605673421236276d5f5787131c47b2628b5
gooberbobber <dabamcdoodle@gmail.com>
2018-02-22 14:13:58 -0300

    Create saveFile and readFile methods

--- app/src/main/java/com/example/ikefluxa/stufflogger/MainActivity.java
+++ app/src/main/java/com/example/ikefluxa/stufflogger/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.ikefluxa.stufflogger;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
+    }
+
+    private void saveFile(String filename, String text) {
+        try {
+            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile(String file) {
+        String text = "";
+        FileInputStream fis;
+        try {
+            fis = openFileInput(file);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text = new String(buffer);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error reading!!", Toast.LENGTH_LONG).show();
+        }
+        return text;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/example/ikefluxa/stufflogger/Subject.java
+++ app/src/main/java/com/example/ikefluxa/stufflogger/Subject.java
@@ -8,4 +8,6 @@ public class Subject {
     String name;
     Boolean checked;
     Boolean onList;
+
+
 }
