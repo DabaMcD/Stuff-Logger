f4040158209695ed3b1d3fae12765bb18a7f5e7a
gooberbobber <dabamcdoodle@gmail.com>
2018-03-03 14:20:48 -0300

    wip

--- app/src/main/java/com/example/ikefluxa/stufflogger/UserActivity.java
+++ app/src/main/java/com/example/ikefluxa/stufflogger/UserActivity.java
@@ -6,16 +6,18 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class UserActivity extends AppCompatActivity {
-    EditText name;
     Button createUser;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        // Define all the things in the xml code
-        name = findViewById(R.id.name);
         createUser = findViewById(R.id.createUser);
+        name = findViewById(R.id.name);
+
+        String[] nicknames = new String[] {"e.g. Bobby Joe", "e.g. Zigzag Dude", "e.g. Ricky", "Nickname goes here", "e.g. Sandy Sanders", "e.g. Carl", "Nick's my nickname, DUH"};
+        name.setHint(nicknames[(int) (Math.random() * nicknames.length)]);
     }
 }
