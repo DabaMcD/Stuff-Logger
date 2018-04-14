e2baad705584ac1182813e0d27223f9a961a2d18
gooberbobber <dabamcdoodle@gmail.com>
2018-04-14 07:13:05 -0300

    In the user list, get the rects and lines in the right positions, fix scrolling bug

---
+++ ToDo.md
@@ -0,0 +1,6 @@
+### To Do ###
+* Fix date sizing bug
+* Fix date initialization
+* Make user list with trash can button on each user
+* Add top bar on AddLogline activity & AddUser activity
+* Fix back button navigation
--- app/src/main/java/com/example/ikefluxa/stufflogger/UsersListView.java
+++ app/src/main/java/com/example/ikefluxa/stufflogger/UsersListView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
@@ -12,24 +13,25 @@ import android.view.View;
 
 public class UsersListView extends View{
     Paint paint = new Paint();
-    float lineThk = Constants.SCREEN_HEIGHT / 300;
+    float lineThk = Constants.SCREEN_HEIGHT / 100;
+    Rect userNameBounds = new Rect();
 
     public UsersListView(Context context) {
         super(context);
         setVerticalScrollBarEnabled(true);
-        setMinimumHeight(100); // ToDo: Fix this babby thing
+        setMinimumHeight((int) (((Constants.SCREEN_HEIGHT / 10) + lineThk) * (Constants.users.size() + 1) - lineThk));
     }
 
     public UsersListView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setVerticalScrollBarEnabled(true);
-        setMinimumHeight(100);
+        setMinimumHeight((int) (((Constants.SCREEN_HEIGHT / 10) + lineThk) * (Constants.users.size() + 1) - lineThk));
     }
 
     public UsersListView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setVerticalScrollBarEnabled(true);
-        setMinimumHeight(100);
+        setMinimumHeight((int) (((Constants.SCREEN_HEIGHT / 10) + lineThk) * (Constants.users.size() + 1) - lineThk));
     }
 
     @Override
@@ -39,18 +41,31 @@ public class UsersListView extends View{
             User user = Constants.users.get(i);
             int bob = (int) (Constants.SCREEN_HEIGHT / 10.0);
             float top = bob * (i + 1) + lineThk * (i + 1);
-            float bottom = (float) ((Constants.SCREEN_HEIGHT / 10.0) * (i + 2) + lineThk * (i + 1));
+            float bottom = top + bob;
 
             // Draw the line in between the users
-//            paint.setColor(Color.DKGRAY);
-//            canvas.drawRect(0, top - lineThk, Constants.SCREEN_WIDTH, top, paint);
+            paint.setColor(Color.DKGRAY);
+            canvas.drawRect(0, top - lineThk, Constants.SCREEN_WIDTH, top, paint);
 
             // Draw the user's box
-            paint.setColor(user.color);
+            paint.setColor(Constants.inverseColor(user.color));
             canvas.drawRect(0, top, Constants.SCREEN_WIDTH, bottom, paint);
+
+            // Draw the user's name
+            paint.setColor(user.color);
+            paint.setTextAlign(Paint.Align.LEFT);
+            paint.setTextSize(Constants.SCREEN_HEIGHT / 17);
+            canvas.drawText(getLongestName(user.name), Constants.SCREEN_WIDTH / 28, top + (Constants.SCREEN_HEIGHT / 20) - (paint.getTextSize() / 3), paint);
         }
 
         super.onDraw(canvas);
+    }
+
+    private String getLongestName(String name) {
+        String result = "";
+        paint.getTextBounds(name, 0, name.length(), userNameBounds);
+
+        return result;
     }
 
     public void draw() {
