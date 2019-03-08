af59855f5ae83835b510525d34fc0300b7c0a8e1
gooberbobber <dabamcdoodle@gmail.com>
2019-03-07 21:19:30 -0300

    Put more date stuff into the MyTime class

--- app/src/main/java/com/stuff/log/ger/Log.java
+++ app/src/main/java/com/stuff/log/ger/Log.java
@@ -9,63 +9,7 @@ class Log implements Serializable {
     String date;
 
     Log() {
-        // Set date vals
-        Calendar cal = Calendar.getInstance();
-        date = // Wed, Feb 10, 2018
-                weekdayFromInt(cal.get(Calendar.DAY_OF_WEEK)) + ", " + // Wed
-                monthFromInt(cal.get(Calendar.MONTH)) + " " + // Feb
-                cal.get(Calendar.DAY_OF_MONTH) + ", " + // 10
-                cal.get(Calendar.YEAR); // 2018
+        date = MyTime.getDisplayableDate();
         logLines = new ArrayList<>();
-    }
-    private String monthFromInt(int month) {
-        switch(month) {
-            case 0:
-                return "Jan";
-            case 1:
-                return "Feb";
-            case 2:
-                return "Mar";
-            case 3:
-                return "Apr";
-            case 4:
-                return "May";
-            case 5:
-                return "Jun";
-            case 6:
-                return "Jul";
-            case 7:
-                return "Aug";
-            case 8:
-                return "Sep";
-            case 9:
-                return "Oct";
-            case 10:
-                return "Nov";
-            case 11:
-                return "Dec";
-            default:
-                return "Invalid input";
-        }
-    }
-    private String weekdayFromInt(int day) {
-        switch(day) {
-            case Calendar.MONDAY:
-                return "Mon";
-            case Calendar.TUESDAY:
-                return "Tue";
-            case Calendar.WEDNESDAY:
-                return "Wed";
-            case Calendar.THURSDAY:
-                return "Thu";
-            case Calendar.FRIDAY:
-                return "Fri";
-            case Calendar.SATURDAY:
-                return "Sat";
-            case Calendar.SUNDAY:
-                return "Sun";
-            default:
-                return "Invalid number";
-        }
     }
 }
--- app/src/main/java/com/stuff/log/ger/MyTime.java
+++ app/src/main/java/com/stuff/log/ger/MyTime.java
@@ -47,4 +47,62 @@ class MyTime implements Serializable {
         }
         return result;
     }
+    static String monthFromInt(int month) {
+        switch(month) {
+            case 0:
+                return "Jan";
+            case 1:
+                return "Feb";
+            case 2:
+                return "Mar";
+            case 3:
+                return "Apr";
+            case 4:
+                return "May";
+            case 5:
+                return "Jun";
+            case 6:
+                return "Jul";
+            case 7:
+                return "Aug";
+            case 8:
+                return "Sep";
+            case 9:
+                return "Oct";
+            case 10:
+                return "Nov";
+            case 11:
+                return "Dec";
+            default:
+                return "Invalid input";
+        }
+    }
+    static String weekdayFromInt(int day) {
+        switch(day) {
+            case Calendar.MONDAY:
+                return "Mon";
+            case Calendar.TUESDAY:
+                return "Tue";
+            case Calendar.WEDNESDAY:
+                return "Wed";
+            case Calendar.THURSDAY:
+                return "Thu";
+            case Calendar.FRIDAY:
+                return "Fri";
+            case Calendar.SATURDAY:
+                return "Sat";
+            case Calendar.SUNDAY:
+                return "Sun";
+            default:
+                return "Invalid number";
+        }
+    }
+    static String getDisplayableDate() {
+        // Returns the date in the form   Mon, Feb 19, 2018
+        Calendar cal = Calendar.getInstance();
+        return weekdayFromInt(cal.get(Calendar.DAY_OF_WEEK)) + ", " + // Mon,
+                monthFromInt(cal.get(Calendar.MONTH)) + " " + // Feb
+                cal.get(Calendar.DAY_OF_MONTH) + ", " + // 19,
+                cal.get(Calendar.YEAR); // 2018
+    }
 }
