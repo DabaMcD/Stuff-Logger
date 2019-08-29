1ff1bf39e074ac35ae3fba1900a739a07e1fb9e5
gooberbobber <dabamcdoodle@gmail.com>
2019-08-28 22:00:37 -0400

    Get ready for the dimensional analysis

--- app/src/main/java/com/stuff/log/ger/LogLogLinesListView.java
+++ app/src/main/java/com/stuff/log/ger/LogLogLinesListView.java
@@ -12,7 +12,7 @@ import android.view.View;
 public class LogLogLinesListView extends View {
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Log log;
-    private float sideLimit;
+    private float sideMargin;
     private float lineGap;
     private float firstLineYpos;
     private float width;
@@ -34,7 +34,7 @@ public class LogLogLinesListView extends View {
         log = Globals.users.get(0).logs.get(Globals.users.get(0).logs.size() - 1);
         lineGap = Screen.height / 10f; // Set min line gap
 
-        updateSideLimit();
+        updateSideMargin();
         updateFirstLineYpos();
         updateTextSize();
 
@@ -59,49 +59,20 @@ public class LogLogLinesListView extends View {
         super.onDraw(canvas);
     }
     private void tweakLineGap() {
-        int longestLogLineIndex = -1;
-        updateSideLimit();
-        updateTextSize();
-        float recordLineWidth = paint.measureText(log.date) + sideLimit * 2f;
-        for(int i = 0; i < log.logLines.size(); i ++) {
-            // Get the length of a log line (without left and right margins)
-            float logLineNameWidth = paint.measureText(log.logLines.get(i).subject.name);
-            float thisLogLineLength = logLineNameWidth + paint.measureText("888N-N") + sideLimit * 2f; // sideLimit * 2 because you gotta account for both sides
+        // Goal: set the line gap correctly so that the length of the longest piece of
+        // text will perfectly fit with perfect margin on both sides
+        //
+        // Once I know how long the text should be, I can reverse calculate through to the
+        // line gap using dimensional analysis using the following ratios:
+        // paint.measureText(text) / paint.getTextSize() = (I'll have to calculate this on the fly)
+        // paint.getTextSize() / lineGap = 0.8
 
-            if(thisLogLineLength > recordLineWidth) { // If it breaks the line length record
-                longestLogLineIndex = i;
-                recordLineWidth = (int) thisLogLineLength;
-            }
-        }
-        while(recordLineWidth > width) {
-            decreaseLineGap();
-            updateTextSize();
-            updateSideLimit();
-
-            if(longestLogLineIndex == -1) {
-                recordLineWidth = paint.measureText(log.date) + sideLimit * 2f; // sideLimit * 2 because you gotta account for both sides
-            } else {
-                String logLineSubjectName = log.logLines.get(longestLogLineIndex).subject.name;
-
-                // logLineNameBounds = bounds of subject name
-                float logLineNameWidth = paint.measureText(logLineSubjectName);
-                recordLineWidth = (int) (sideLimit * 2f + logLineNameWidth + paint.measureText("888N-N"));
-            }
-        }
-        updateFirstLineYpos();
-        while(log.logLines.size() * lineGap + firstLineYpos /* and just for extra buffer at the bottom: */ + lineGap * bufferAtBottom > Screen.height) {
-            decreaseLineGap();
-            updateFirstLineYpos();
-        }
-    }
-    private void decreaseLineGap() {
-        lineGap /= 1.01d;
     }
     private void updateTextSize() {
         paint.setTextSize((float) (lineGap * 0.8d));
     }
-    private void updateSideLimit() {
-        sideLimit = (float) (lineGap / 2d);
+    private void updateSideMargin() {
+        sideMargin = (float) (lineGap / 2d);
     }
     private void updateFirstLineYpos() {
         firstLineYpos = (float) (lineGap * 1.5);
@@ -116,13 +87,13 @@ public class LogLogLinesListView extends View {
     private void drawDate(Canvas canvas) {
         paint.setColor(Color.DKGRAY);
         paint.setTextAlign(Paint.Align.LEFT);
-        canvas.drawText(log.date, sideLimit, lineGap + paint.getTextSize() / 3f, paint);
+        canvas.drawText(log.date, sideMargin, lineGap + paint.getTextSize() / 3f, paint);
     }
     private void drawLogLines(Canvas canvas) {
         /* THE X-POSITIONING CHART (if you can call it a chart)
          * Log lines are drawn in the following manner:
          *               137 - Code
-         *<--sideLimit-->888N-NCode<--sideLimit-->
+         *<--sideMargin-->888N-NCode<--sideMargin-->
          *
          * Do you follow the above? The upper text is what will actually be shown on the log. The lower text is the spacing I use to get everything aligned.
          */
@@ -139,31 +110,31 @@ public class LogLogLinesListView extends View {
             String dadTime = log.logLines.get(i).startTime.getDadTime();
             canvas.drawText(
                     String.valueOf(dadTime.charAt(0)),
-                    sideLimit,// Correct x-position according to the chart above
+                    sideMargin,// Correct x-position according to the chart above
                     txtYpos,
                     paint
             );
             canvas.drawText(
                     String.valueOf(dadTime.charAt(1)),
-                    sideLimit + paint.measureText("8"), // Correct x-position according to the chart above
+                    sideMargin + paint.measureText("8"), // Correct x-position according to the chart above
                     txtYpos,
                     paint
             );
             canvas.drawText(
                     String.valueOf(dadTime.charAt(2)),
-                    sideLimit + paint.measureText("88"), // Correct x-position according to the chart above
+                    sideMargin + paint.measureText("88"), // Correct x-position according to the chart above
                     txtYpos,
                     paint
             );
             canvas.drawText(
                     "-",
-                    sideLimit + paint.measureText("888N"), // Correct x-position according to the chart above
+                    sideMargin + paint.measureText("888N"), // Correct x-position according to the chart above
                     txtYpos,
                     paint
             );
             canvas.drawText(
                     log.logLines.get(i).subject.name,
-                    sideLimit + paint.measureText("888N-N"), // Correct x-position according to the chart above
+                    sideMargin + paint.measureText("888N-N"), // Correct x-position according to the chart above
                     txtYpos,
                     paint
             );
